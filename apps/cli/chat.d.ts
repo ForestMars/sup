c1a134d4c417c6f2ddc55b06f82e570575827080
ForestMars <themarsgroup+gh@gmail.com>
2026-03-03 22:49:00 -0500

    rm compiled

--- apps/cli/chat.d.ts
+++
@@ -1,7 +0,0 @@
-/**
- * Main chat loop logic, exported for integration testing.
- * The readline interface is created inside the function to allow for
- * clean mocking and to prevent top-level sidffects during testing.
- */
-export declare function startChat(): Promise<void>;
-//# sourceMappingURL=chat.d.ts.map
