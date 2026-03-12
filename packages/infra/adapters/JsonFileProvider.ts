6ea094f39f4328627f8880baa980b89bb3e6c14c
ForestMars <themarsgroup+gh@gmail.com>
2026-03-12 13:10:38 -0400

    file=provider

--- packages/infra/adapters/JsonFileProvider.ts
+++ packages/infra/adapters/JsonFileProvider.ts
@@ -16,8 +16,8 @@ export class JsonFileProvider implements Provider {
   async resolveBooleanEvaluation(
     flagKey: string,
     defaultValue: boolean,
-    context: EvaluationContext,
-    logger: Logger,
+    _context: EvaluationContext,
+    _logger: Logger,
   ): Promise<ResolutionDetails<boolean>> {
     const value =
       (flagsConfig[flagKey as keyof typeof flagsConfig] as any)?.enabled ??
@@ -28,8 +28,8 @@ export class JsonFileProvider implements Provider {
   async resolveStringEvaluation(
     flagKey: string,
     defaultValue: string,
-    context: EvaluationContext,
-    logger: Logger,
+    _context: EvaluationContext,
+    _logger: Logger,
   ): Promise<ResolutionDetails<string>> {
     const value =
       (flagsConfig[flagKey as keyof typeof flagsConfig] as any)?.value ??
@@ -40,8 +40,8 @@ export class JsonFileProvider implements Provider {
   async resolveNumberEvaluation(
     flagKey: string,
     defaultValue: number,
-    context: EvaluationContext,
-    logger: Logger,
+    _context: EvaluationContext,
+    _logger: Logger,
   ): Promise<ResolutionDetails<number>> {
     const value =
       (flagsConfig[flagKey as keyof typeof flagsConfig] as any)?.value ??
@@ -52,8 +52,8 @@ export class JsonFileProvider implements Provider {
   async resolveObjectEvaluation<T extends JsonValue>(
     flagKey: string,
     defaultValue: T,
-    context: EvaluationContext,
-    logger: Logger,
+    _context: EvaluationContext,
+    _logger: Logger,
   ): Promise<ResolutionDetails<T>> {
     const value =
       (flagsConfig[flagKey as keyof typeof flagsConfig] as any)?.value ??
