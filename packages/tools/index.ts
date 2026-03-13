06d73c7e1f07bd0234b406cad1e915b42662b894
ForestMars <themarsgroup+gh@gmail.com>
2026-03-13 12:02:43 -0400

    export registry

--- packages/tools/index.ts
+++ packages/tools/index.ts
@@ -2,10 +2,23 @@
  * @file index.ts
  * @description Central registry for all support tools.
  */
-import { entityLookupTool } from './order-tools';
+import registry from "./registry.json" assert { type: "json" };
+import { runTool } from "./loader";
+
+export { runTool };
+export const tools = registry;
+
+// This was our old smoke test
+// import { entityLookupTool } from './order-tools';
+
+// This was just a placeholder 
 // import { itTicketTool } from './it-tools';
+
+// Not sure 
 import adapters, { entityLookupAdapter } from './adapters';
 // ... other 8 tools
+
+/*
 
 export const toolRegistry = [
   entityLookupTool,
@@ -15,10 +28,15 @@ export const toolRegistry = [
 
 export type Tool = (typeof toolRegistry)[number];
 
+*/ 
 export { adapters, entityLookupAdapter };
 // export { baseTools } from './order-tools';
+
+/* 
 export {
   baseTools,
   billingTools,
   resolutionTools,
 } from './order-tools';
+
+*/
